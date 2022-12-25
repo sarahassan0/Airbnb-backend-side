82aa5b3333d139a16cb8361cac182ea51511ebb8
Sarah hassan <sarraree5@gmail.com>
2022-12-25 05:03:28 +0200

    edit reservation module

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -6,20 +6,19 @@ dotenv.config();
 
 function userAuth(req, res, next) {
     const token = req.headers.authorization
-    console.log("token",token),
-    console.log("userId",req.body);
-    jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
-        if (decoded) {
-            req.userID = decoded.data.userID
-            console.log("sssssss",req.userID);
+    console.log("token", token),
+        jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+            if (decoded) {
+                req.userID = decoded.data.userID
+                console.log("sssssss", req.userID);
 
-            next()
-        }
-        if (err) {
-            console.log("dddddddd",req.userID);
-            res.status(401).json('Unauthorized User')
-        }
-    });
+                next()
+            }
+            if (err) {
+                console.log("dddddddd", req.userID);
+                res.status(401).json('Unauthorized User')
+            }
+        });
 
 }
 
